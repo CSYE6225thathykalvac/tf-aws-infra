ef2b19e37dfaf99b42e13a2b628d261cea229c30
Charan Reddy Thathykalva <thathykalva.c@northeastern.edu>
2025-02-25 15:41:10 -0500

    Update subnets.tf

--- terraform-networking/subnets.tf
+++ terraform-networking/subnets.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "public" {
   count                   = length(var.availability_zones)
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = aws_vpc.main.bad
   cidr_block              = var.public_subnet_cidrs[count.index]
   availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
